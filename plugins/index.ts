517560b9f58180ea262235e2a38363e6753e912e
sroxck <642261686@qq.com>
2024-09-02 16:46:19 +0800

    fix: type

--- plugins/index.ts
+++ plugins/index.ts
@@ -2,7 +2,7 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import vue from "@vitejs/plugin-vue";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import { PluginOption } from "vite";
+import { type PluginOption } from "vite";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 export const plugins: PluginOption[] = [
   vue(),
